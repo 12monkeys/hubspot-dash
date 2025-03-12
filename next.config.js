700c88c86d2d2386041ea12fc4e360bc0de84d1b
12monkeys <twelvemonkeys@gmail.com>
2025-03-12 12:22:47 +0100

    Add root layout and global styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  transpilePackages: ['@tremor/react'],
 };
 
 module.exports = nextConfig; 
