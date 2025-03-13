79c258fdbec782ab451e1a7b9bcbcc800f9412ef
12monkeys <twelvemonkeys@gmail.com>
2025-03-13 23:24:28 +0100

    Eliminar completamente Prisma del proyecto

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@tremor/react"],
-  webpack: (config, { isServer }) => {
-    // Añadir alias para reemplazar el módulo de Prisma con nuestro mock
-    config.resolve.alias['@prisma/client'] = path.resolve(__dirname, './app/lib/prisma-mock.js');
-    
-    return config;
-  },
   // Esta configuración solo aplica a rutas que usan PrismaClient
   experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
-  env: {
-    DATABASE_URL: "file:./dev.db"
+    serverComponentsExternalPackages: [],
   }
 }
 
